440983e3d3d337a921804a6586e0210830561469
Tynirra Kellum <ebony.kellum@knights.ucf.edu>
2012-01-26 23:17:22 -0500

    this is the 1st commit of the file

---
+++ MyFirstGame/src/GameGUI.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class GameGUI {
+
+	private JFrame frame;
+	private 
+	
+	
+	
+	public GameGUI(){
+		frame = new JFrame ("My FIrst Game");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		
+		
+		
+		
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
